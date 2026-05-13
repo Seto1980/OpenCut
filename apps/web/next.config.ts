2c7cc9e1863c416e74dc0b8202036970f37b257e
Seto1980 <tennjoubasu@gmail.com>
2026-05-13 11:50:50 +0900

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,25 +5,13 @@ import { withContentCollections } from "@content-collections/next";
 const nextConfig = {
 	typescript: { ignoreBuildErrors: true },
 	eslint: { ignoreDuringBuilds: true },
-	compiler: {
-		removeConsole: process.env.NODE_ENV === "production",
-	},
+	compiler: { removeConsole: process.env.NODE_ENV === "production" },
 	reactStrictMode: true,
-	productionBrowserSourceMaps: true,
 	images: {
 		unoptimized: true,
-		remotePatterns: [
-			{ protocol: "https", hostname: "plus.unsplash.com" },
-			{ protocol: "https", hostname: "images.unsplash.com" },
-			{ protocol: "https", hostname: "images.marblecms.com" },
-			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
-			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
-			{ protocol: "https", hostname: "api.iconify.design" },
-			{ protocol: "https", hostname: "api.simplesvg.com" },
-			{ protocol: "https", hostname: "api.unisvg.com" },
-			{ protocol: "https", hostname: "cdn.brandfetch.io" },
-		],
+		remotePatterns: [{ protocol: "https", hostname: "**" }],
 	},
 };
 
+// ここで (as any) を使い、エラーを強制スルーさせます
 export default (withContentCollections as any)((withBotId as any)(nextConfig as any));
